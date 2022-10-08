469a53a5f05e6afc02a3ee6c7512d567bfc4538f
SamDev-7 <hello@saml.is-a.dev>
2022-10-08 12:03:35 -0400

    Basic site template

---
+++ src/lib/site/Footer.svelte
@@ -0,0 +1,5 @@
+<div class="text-center py-4 px-4">
+	<p class="text-xs text-slate-500 dark:text-slate-400">
+		Copyright © {new Date().getFullYear()} by Sam Liu. <a rel="noopener" class="secondary-inline-url" href="https://github.com/SamDev-7/ui-components">See the source code and license on Github</a>.
+	</p>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import '../app.css';
+
+    import Navbar from '$lib/site/Navbar.svelte';
+    import Footer from '$lib/site/Footer.svelte';
+
+    import { scroll } from '$lib/site/utils/scroll';
+
+	function scrollHandler() {
+		scroll.set(window.scrollY);
+	}
+
+</script>
+
+<svelte:window on:scroll={scrollHandler} />
+
+<Navbar />
+
+<main class="bg-white dark:bg-slate-900">
+	<slot />
+</main>
+
+<footer class="bg-white dark:bg-slate-900">
+    <Footer />
+</footer>
