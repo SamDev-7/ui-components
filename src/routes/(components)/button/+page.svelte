dfc907bf1954e73df09e306e28e891cd32990367
SamDev-7 <hello@samliu.dev>
2022-10-18 23:45:42 -0400

    Update

---
+++ src/routes/(components)/button/+page.svelte
@@ -0,0 +1,60 @@
+<svelte:head>
+    <title>Button - UI Components</title>
+</svelte:head>
+
+<!-- Title -->
+<div class="py-32 text-center bg-gradient-to-br from-indigo-100 to-blue-300 dark:from-cyan-700 dark:to-blue-900">
+    <h1 class="text-7xl font-medium dark:text-white">Buttons</h1>
+</div>
+
+<div class="w-full flex">
+    <div class="m-auto min-h-full py-32 grid gap-8 grid-cols-5 grid-rows-2 place-items-center">
+            <button class="bg-blue-500 hover:bg-blue-400 dark:hover:bg-blue-600 text-white py-2 px-4 rounded tracking-wide active:shadow-inner">
+                Primary Button
+            </button>
+            <button class="bg-gray-500 hover:bg-gray-400 dark:hover:bg-gray-600 text-white py-2 px-4 rounded tracking-wide active:shadow-inner">
+                Secondary Button
+            </button>
+            <button class="bg-red-500 hover:bg-red-400 dark:hover:bg-red-600 text-white py-2 px-4 rounded tracking-wide active:shadow-inner">
+                Danger Button
+            </button>
+            <button class="bg-yellow-500 hover:bg-yellow-400 dark:hover:bg-yellow-600 text-white py-2 px-4 rounded tracking-wide active:shadow-inner">
+                Warning Button
+            </button>
+            <button class="bg-green-500 hover:bg-green-400 dark:hover:bg-green-600 text-white py-2 px-4 rounded tracking-wide active:shadow-inner">
+                Success Button
+            </button>
+
+            <button class="bg-transparent hover:bg-blue-500 text-blue-500 hover:text-white py-[calc(0.5rem-2px)] px-[calc(1rem-2px)] border-2 border-blue-500 rounded tracking-wide active:shadow-inner">
+                Primary Outline
+            </button>
+            <button class="bg-transparent hover:bg-gray-500 text-gray-500 hover:text-white py-[calc(0.5rem-2px)] px-[calc(1rem-2px)] border-2 border-gray-500 rounded tracking-wide active:shadow-inner">
+                Secondary Outline
+            </button>
+            <button class="bg-transparent hover:bg-red-500 text-red-500 hover:text-white py-[calc(0.5rem-2px)] px-[calc(1rem-2px)] border-2 border-red-500 rounded tracking-wide active:shadow-inner">
+                Danger Outline
+            </button>
+            <button class="bg-transparent hover:bg-yellow-500 text-yellow-500 hover:text-white py-[calc(0.5rem-2px)] px-[calc(1rem-2px)] border-2 border-yellow-500 rounded tracking-wide active:shadow-inner">
+                Warning Outline
+            </button>
+            <button class="bg-transparent hover:bg-green-500 text-green-500 hover:text-white py-[calc(0.5rem-2px)] px-[calc(1rem-2px)] border-2 border-green-500 rounded tracking-wide active:shadow-inner">
+                Success Outline
+            </button>
+
+            <button class="bg-blue-300 text-white py-2 px-4 rounded tracking-wide cursor-not-allowed" disabled>
+                Disabled Primary
+            </button>
+            <button class="bg-gray-300 text-white py-2 px-4 rounded tracking-wide cursor-not-allowed" disabled>
+                Disabled Secondary
+            </button>
+            <button class="bg-red-300 text-white py-2 px-4 rounded tracking-wide cursor-not-allowed" disabled>
+                Disabled Danger
+            </button>
+            <button class="bg-yellow-300 text-white py-2 px-4 rounded tracking-wide cursor-not-allowed" disabled>
+                Disabled Warning
+            </button>
+            <button class="bg-green-300 text-white py-2 px-4 rounded tracking-wide cursor-not-allowed" disabled>
+                Disabled Success
+            </button>
+        </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,19 @@
 <script>
     import ComponentCard from "$lib/site/ComponentCard.svelte";
 
+    import Navbar from '$lib/site/Navbar.svelte';
+    import Footer from '$lib/site/Footer.svelte';
+
 </script>
 
 <svelte:head>
     <title>UI Components - Sam Liu</title>
 </svelte:head>
 
+<Navbar />
+
 <!-- Title -->
-<div class="py-[20vh] text-center bg-gradient-to-br from-indigo-100 to-blue-300 dark:from-cyan-700 dark:to-blue-900">
+<div class="py-32 text-center bg-gradient-to-br from-indigo-100 to-blue-300 dark:from-cyan-700 dark:to-blue-900">
     <h1 class="text-7xl font-medium dark:text-white">UI Components</h1>
     <p class="mt-6 text-lg text-slate-600 dark:text-slate-300">A collection of my experimentation with building UI components.</p>
 </div>
@@ -16,10 +21,14 @@
 <!-- Cards -->
 <div class="py-16 px-64 grid grid-cols-3 gap-12 xl:-2xl:px-32 xs:-xl:px-16 md:-lg:grid-cols-2 -md:grid-cols-1 2xs:-xs:px-8 -2xs:px-0 -2xs:gap-6 -2xs:divide-y-2 dark:divide-slate-700">
     <!-- CC0 placeholder image by Ryan Klaus -->
-    <ComponentCard title="Button" description="A button component with a variety of styles and sizes." thumbnail="https://images.pexels.com/photos/13235188/pexels-photo-13235188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" href="/" />
+    <ComponentCard title="Button" description="A button component with a variety of styles and sizes." thumbnail="https://images.pexels.com/photos/13235188/pexels-photo-13235188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" href="/button" />
     <ComponentCard title="Button" description="A button component with a variety of styles and sizes." thumbnail="https://images.pexels.com/photos/13235188/pexels-photo-13235188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" href="/" />
     <ComponentCard title="Button" description="A button component with a variety of styles and sizes." thumbnail="https://images.pexels.com/photos/13235188/pexels-photo-13235188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" href="/" />
     <ComponentCard title="Button" description="A button component with a variety of styles and sizes." thumbnail="https://images.pexels.com/photos/13235188/pexels-photo-13235188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" href="/" />
     <ComponentCard title="Button" description="A button component with a variety of styles and sizes." thumbnail="https://images.pexels.com/photos/13235188/pexels-photo-13235188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" href="/" />
     <ComponentCard title="Button" description="A button component with a variety of styles and sizes." thumbnail="https://images.pexels.com/photos/13235188/pexels-photo-13235188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" href="/" />
 </div>
+
+<footer class="bg-white dark:bg-slate-900">
+    <Footer />
+</footer>
